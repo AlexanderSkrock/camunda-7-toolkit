5be6e7cfadf1502f53936e75f97070212c6e0860
Alexander Skrock <alexanderskrock@gmx.de>
2025-02-17 23:25:27 +0100

    feat(ui): integration of frontend framework vaadin

--- src/main/java/dev/skrock/camunda/Camunda7ToolkitApplication.java
+++ src/main/java/dev/skrock/camunda/Camunda7ToolkitApplication.java
@@ -4,9 +4,7 @@ import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 import dev.skrock.camunda.util.SSLUtil;
 
@@ -15,11 +13,6 @@ public class Camunda7ToolkitApplication {
 
     public static void main(String... args) throws NoSuchAlgorithmException, KeyManagementException {
         SSLUtil.turnOffSslChecking();
-
-        SpringApplicationBuilder appBuilder = new SpringApplicationBuilder()
-                .sources(Camunda7ToolkitApplication.class)
-                .web(WebApplicationType.NONE);
-
-        appBuilder.run(args);
+        SpringApplication.run(Camunda7ToolkitApplication.class);
     }
 }
---
+++ src/main/java/dev/skrock/camunda/ui/MainView.java
@@ -0,0 +1,21 @@
+package dev.skrock.camunda.ui;
+
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.spring.annotation.SpringComponent;
+import dev.skrock.camunda.actions.ToolkitActionsExecutor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Route
+@SpringComponent
+public class MainView extends VerticalLayout {
+
+    @Autowired
+    public MainView(ToolkitActionsExecutor executor) {
+        add(new Button("Execute", (ComponentEventListener<ClickEvent<Button>>) buttonClickEvent -> executor.executeActions()));
+    }
+
+}
