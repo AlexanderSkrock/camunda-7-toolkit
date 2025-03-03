38875d7178382558be5a4ffbd7f7a2d0eff7946f
Alexander Skrock <alexanderskrock@gmx.de>
2025-03-03 08:37:39 +0100

    feat(export) added download all functionality

--- src/main/java/dev/skrock/camunda/toolkit/api/ProcessDefinitionService.java
+++ src/main/java/dev/skrock/camunda/toolkit/api/ProcessDefinitionService.java
@@ -11,10 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -77,5 +74,13 @@ public class ProcessDefinitionService {
         ResponseUtil.checkResponse(diagramResponse);
 
         return ProcessDefinitionModel.ofDto(diagramResponse.getBody());
+    }
+
+    public List<ProcessDefinitionModel> getProcessDefinitionModels(Set<ProcessDefinition> definitions) throws ResponseException {
+        List<ProcessDefinitionModel> models = new LinkedList<>();
+        for (ProcessDefinition definition : definitions) {
+            models.add(getProcessDefinitionModel(definition));
+        }
+        return models;
     }
 }
